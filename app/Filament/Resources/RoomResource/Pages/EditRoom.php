85160120073a42afd3ec91bd54dad3cc2445164d
Vova <vkarchevskyi10@gmail.com>
2023-12-10 13:45:19 +0200

    Feat: create RoomResource. Add relationship for Room model.

---
+++ app/Filament/Resources/RoomResource/Pages/EditRoom.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\RoomResource\Pages;
+
+use App\Filament\Resources\RoomResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditRoom extends EditRecord
+{
+    protected static string $resource = RoomResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -27,6 +28,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @method static \Illuminate\Database\Eloquent\Builder|Room whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Room withTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|Room withoutTrashed()
+ * @property-read \App\Models\RoomType $roomType
  * @mixin \Eloquent
  */
 class Room extends Model
@@ -37,4 +39,9 @@ class Room extends Model
         'room_type_id',
         'name'
     ];
+
+    public function roomType(): BelongsTo
+    {
+        return $this->belongsTo(RoomType::class);
+    }
 }
