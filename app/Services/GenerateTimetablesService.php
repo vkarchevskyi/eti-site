40e2feb876fade5802457cf47a70678e218911f6
Vova <vkarchevskyi10@gmail.com>
2023-12-20 19:00:37 +0200

    Feat: add deleted lessons cleaning. Update timetable according to lessons.

--- app/Services/GenerateTimetablesService.php
+++ app/Services/GenerateTimetablesService.php
@@ -27,30 +27,43 @@ class GenerateTimetablesService
                 ->where('studying_end_date', '>=', Carbon::now())
                 ->first();
 
+            $now = Carbon::now();
+            $semesterStart = Carbon::make($semester->studying_start_date);
+
             for (
                 /* @var Carbon $date */
-                $date = Carbon::make($semester->studying_start_date);
+                $date = $now > $semesterStart ? $now : $semesterStart;
                 $date <= $semester->studying_end_date;
                 $date->addDay()
             ) {
-                /* @var Lesson $lesson*/
+                /* @var Lesson $lesson */
                 foreach ($lessonsPerDay[$date->dayOfWeekIso] ?? [] as $lesson) {
-                    Timetable::query()->firstOrCreate([
+                    $record = Timetable::query()->firstOrCreate([
                         'semester_id' => $semester->id,
-                        'start_at' => $date->setTimeFromTimeString($lesson->time_from),
-                        'end_at' => $date->setTimeFromTimeString($lesson->time_to),
+                        'start_at' => Carbon::make($date)->setTimeFromTimeString($lesson->time_from),
+                        'end_at' => Carbon::make($date)->setTimeFromTimeString($lesson->time_to),
                         'order' => $lesson->order,
-                        'is_numerator' => $lesson->is_numerator,
                         'day_of_week_id' => $lesson->day_of_week_id,
-                        'room_id' => $lesson->room_id,
                         'group_id' => $lesson->group_id,
-                        'subgroup_id' => $lesson->subgroup_id,
-                        'type_of_lesson_id' => $lesson->type_of_lesson_id,
+                    ]);
+
+                    $record->update([
+                        'lesson_id' => $lesson->id,
                         'teacher_id' => $lesson->teacher_id,
+                        'type_of_lesson_id' => $lesson->type_of_lesson_id,
+                        'is_numerator' => $lesson->is_numerator,
+                        'room_id' => $lesson->room_id,
+                        'subgroup_id' => $lesson->subgroup_id,
                         'course_id' => $lesson->course_id
                     ]);
                 }
             }
+
+            Timetable::query()
+                ->select('timetables.*')
+                ->join('lessons as l', 'l.id', '=', 'timetables.lesson_id')
+                ->whereNotNull('l.deleted_at')
+                ->delete();
 
             DB::commit();
         } catch (\Exception $e) {
