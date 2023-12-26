b8e143c952b6274606b88064869d426e72e57e02
Vova <vkarchevskyi10@gmail.com>
2023-12-26 09:23:22 +0200

    Fix: refactor code in TimetableShow.php

--- app/Livewire/TimetableShow.php
+++ app/Livewire/TimetableShow.php
@@ -23,7 +23,7 @@ class TimetableShow extends Component
     public ?Group $activeGroup;
     public ?Subgroup $activeSubgroup;
 
-    public function mount(?User $user = null)
+    public function mount(?User $user = null): void
     {
         $this->groups = Group::with('subgroups')->get();
 
@@ -39,22 +39,18 @@ class TimetableShow extends Component
 
     public function render()
     {
-
-
-        $now = Carbon::now();
-
-        $endOfWeek = Carbon::make($now);
-        while (!$endOfWeek->isSunday()) {
-            $endOfWeek->addDay();
-        }
-
-        $endAt = Carbon::make($now)->addDays(7);
-
         $semester = Semester::query()
             ->where('studying_end_date', '>=', Carbon::now())
             ->first();
 
         $firstWeek = Carbon::make($semester->studying_start_date)->weekOfYear;
+
+        $now = Carbon::now();
+        $endAt = Carbon::make($now)->addDays(7);
+        $endOfWeek = Carbon::make($now);
+        while (!$endOfWeek->isSunday()) {
+            $endOfWeek->addDay();
+        }
 
         /* @var Collection $timetables */
         $timetables = Timetable::with(['group', 'teacher', 'typeOfLesson', 'course', 'subgroup', 'room'])
