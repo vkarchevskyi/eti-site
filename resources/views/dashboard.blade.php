3c4dea38c1465162b3c3d09e43ed8ad97b818aa7
Vova <vkarchevskyi10@gmail.com>
2023-12-26 09:19:29 +0200

    Feat: add layout to dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,19 @@
 <x-app-layout>
     <div class="py-12 mt-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+            <div class="bg-white mb-4 overflow-hidden shadow-sm sm:rounded-lg border">
+                <div class="p-6 text-gray-900">
+                    Привіт, {{ auth()->user()->name }}!
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Вийти
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </div>
             </div>
+            <livewire:timetable-show :user="auth()->user()" />
         </div>
     </div>
 </x-app-layout>
